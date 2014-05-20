095109f15c8b8baff329429333b6db1eb5546292
Thomas Storey <storey.thomas@gmail.com>
2014-05-19 23:20:15 -0400

    background.js implemented, persistent state

--- background.js
+++ background.js
@@ -1,22 +1,4 @@
-// When the extension is installed or upgraded ...
-chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        // That fires when a page's URL contains a 'g' ...
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'facebook' },
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
-  });
-});
+var check;
 
 function showPageAction(tabId, changeInfo, tab){
   if(tab.url.indexOf("facebook") != -1){
--- popup.js
+++ popup.js
@@ -5,18 +5,20 @@ var Program = function(){
 	var init = function(){
 		var checkbox = document.getElementById('toggle');
 		var bg = chrome.extension.getBackgroundPage();
-		
+		checkbox.checked = bg.check;
 		checkbox.addEventListener('click', function(){
 			if(checkbox.checked){
 				chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 					chrome.tabs.sendMessage(tabs[0].id, {greeting:"ON"}, function(response){
 						console.log(response.farewell);
+						bg.check = true;
 					});
 				});
 			}else if(!checkbox.checked){
 				chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 					chrome.tabs.sendMessage(tabs[0].id, {greeting:"OFF"}, function(response){
 						console.log(response.farewell);
+						bg.check = false;
 					});
 				});
 			}
